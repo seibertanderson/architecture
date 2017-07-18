bc9e0e9b6f170657155c72d71574164c14a036d3
Anderson <andersonseibert0@gmail.com>
2017-07-18 11:29:41 -0300

    3º

---
+++ architecture/Web/js/config.js
@@ -0,0 +1,42 @@
+﻿/**
+* Main AngularJS Web Application
+*/
+var app = angular.module('app', [
+'ngRoute'
+]);
+
+/**
+* Configure the Routes
+*/
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+    // Home
+    .when("/", {
+        templateUrl: "views/home.html", 
+        controller: "PageCtrl"
+    })
+    // Pages
+    .when("/about", {
+        templateUrl: "views/about.html",
+        controller: "PageCtrl"
+    })
+    .when("/faq", {
+        templateUrl: "views/faq.html",
+        controller: "PageCtrl"
+    })
+    /* etc… routes to other pages… */
+    // Blog
+    .when("/blog", {
+        templateUrl: "views/blog.html",
+        controller: "BlogCtrl"
+    })
+    .when("/blog/post", {
+        templateUrl: "views/blog_item.html",
+        controller: "BlogCtrl"
+    })
+    // else 404
+    .otherwise("/404", {
+        templateUrl: "views/404.html",
+        controller: "PageCtrl"
+    });
+}]);
