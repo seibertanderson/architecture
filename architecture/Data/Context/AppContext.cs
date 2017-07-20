fbe98866b49ac08308ce3e0a4b41078e544de00a
Anderson <andersonseibert0@gmail.com>
2017-07-20 15:39:03 -0300

    No Commit Message

---
+++ architecture/Data/Context/AppContext.cs
@@ -0,0 +1,36 @@
+﻿using Data.EntityConfig;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Context
+{
+    public class AppContext : DbContext
+    {
+        public AppContext() : base("name=NomeBanco")
+        {
+            this.Configuration.LazyLoadingEnabled = true;
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //REMOVE CONVENÇÃO DE NOME EM PLURAL NA BASE DE DADOS (ENTITY FRAMEWORK DOESN'T SPEEK PORTUGUESE ;] )
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            //CONFIGURAÇÕES PADRÕES PARA PROPRIEDADES STRING -> VARCHAR(255)
+            modelBuilder.Properties<string>().Configure(el => el.HasColumnType("varchar"));
+            modelBuilder.Properties<string>().Configure(el => el.HasMaxLength(255));
+
+            modelBuilder.Configurations.Add(new ProdutoConfig());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ architecture/Domain/Entities/Produto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Produto
+    {
+        public long pro_id { get; set; }
+        public string pro_nome { get; set; }
+    }
+}
